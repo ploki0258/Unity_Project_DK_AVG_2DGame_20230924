78a839d564ceb203ed9f2a01836bae659eb6eb6b
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-10-24 21:57:34 +0800

    Update 20231024修改對話系統&新增對話

--- Assets/Script/DialogueData.cs
+++ Assets/Script/DialogueData.cs
@@ -9,8 +9,38 @@
 [CreateAssetMenu(menuName = "Create_New_DialogueData", fileName = "New_DialogueData")]
 public class DialogueData : ScriptableObject
 {
+	[Header("對話編號")]
+	public int dialogueID = 0;
+	[Header("對話編號跳轉至"), Tooltip("可跳轉至對話或選項")]
+	public int toDialogueID;
+	[Header("選項編號")]
+	public int optionID;
+	[Header("對話種類")]
+	public DialogueType dialogueType = DialogueType.對話;
 	[Header("對話者名稱")]
 	public string dialogueTalkerName = "";
 	[Header("對話內容陣列"), TextArea(2, 5)]
 	public string[] dialogueContents;
+	//[Header("選項個數"), Range(0, 5)]
+	//public int optionCount = 0;
+	[Header("選項內容"), TextArea(2, 5)]
+	public string[] optionContents = { "" };
+	[Header("效果種類")]
+	public EffectType effectType = EffectType.無效果;
+	[Header("效果對象名稱")]
+	public string effectTargetName = "";
+	[Header("好感度"), Range(0, 100)]
+	public float favorability;
+	[Header("體力值"), Range(0, 100)]
+	public float strength;
+}
+
+public enum DialogueType
+{
+	對話, 選項, 結束
+}
+
+public enum EffectType
+{
+	無效果, 好感度, 體力值
 }
--- Assets/Script/DialogueSystem.cs
+++ Assets/Script/DialogueSystem.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 /// 對話系統：
@@ -14,9 +15,14 @@ public class DialogueSystem : MonoBehaviour
 	// 將 DialogSystem 設定為單例模式
 	public static DialogueSystem instance = null;
 
+	[Tooltip("對話資料陣列")]
+	public DialogueData[] dialogueDataArrary = new DialogueData[0];
+
 	#region 欄位
 	[Header("對話資料")]
-	public DialogueData dialogueData;
+	public DialogueData[] dialogueData;
+	[Header("當前對話編號")]
+	public int currentDialogueID = 0;
 	[Header("對話間隔"), Range(0, 2)]
 	public float interval = 0.2f;
 	[Header("對話框")]
@@ -27,6 +33,14 @@ public class DialogueSystem : MonoBehaviour
 	public TextMeshProUGUI textContent;
 	[Header("對話繼續圖示")]
 	public GameObject continueIcon = null;
+	[Header("對話人物圖示_左")]
+	public Image dialogueImage_left;
+	[Header("對話人物圖示_右")]
+	public Image dialogueImage_right;
+	[Header("對話選項按鈕")]
+	public GameObject optionButton = null;
+	[Header("對話選項座標")]
+	public RectTransform dialoguePos = null;
 
 	[Tooltip("是否自動播放")]
 	private bool isAutoplay = false;
@@ -38,7 +52,11 @@ public class DialogueSystem : MonoBehaviour
 	{
 		instance = this;    // 讓單例等於自己
 							//talkUI.alpha = 0f;  // 一開始隱藏對話框 α值為0
+		dialogueDataArrary = Resources.LoadAll<DialogueData>("");
+	}
 
+	private void Start()
+	{
 		StartDialogue();
 	}
 
@@ -80,63 +98,84 @@ public class DialogueSystem : MonoBehaviour
 	{
 		// 顯示對話畫布 透明度為1
 		dialogieUI.alpha = 1;
-		// 更新對話者名稱
-		textTalker.text = dialogueData.dialogueTalkerName;
 		// 清空對話內容
 		textContent.text = "";
+		// 隱藏對話選項按鈕
+		optionButton.SetActive(false);
 
-		// 第一個迴圈跑總共有幾個對話內容
-		// 迴圈初始值不可為重複
-		for (int i = 0; i < dialogueData.dialogueContents.Length; i++)
+		// 第一個迴圈跑總共有幾個對話資料
+		for (int i = 0; i < dialogueData.Length; i++)
 		{
-			// 第二個迴圈跑該總對話內容中共有幾個字
-			for (int j = 0; j < dialogueData.dialogueContents[i].Length; j++)
+			// 如果第i個對話資料.對話類別為"對話" 且 當前ID 等於 第i個對話資料.對話編號 的話 才執行
+			if (dialogueData[i].dialogueType == DialogueType.對話 && currentDialogueID == dialogueData[i].dialogueID)
 			{
-				Debug.Log(dialogueData.dialogueContents[i][j]);
-				// 更新對話內容
-				textContent.text += dialogueData.dialogueContents[i][j];
-				// 打字間隔
-				yield return new WaitForSeconds(interval);
-			}
+				optionButton.SetActive(false);
 
-			// 每段對話完成後顯示繼續圖示
-			continueIcon.SetActive(true);
+				// 更新對話者名稱
+				textTalker.text = dialogueData[i].dialogueTalkerName;
 
-			// 如果 沒有隱藏對話框的話
-			if (isHideDialogue == false)
-			{
-				// 如果 沒有自動播放的話
-				if (isAutoplay == false)
+				// 第二個迴圈跑第i個對話資料總共有幾個對話內容
+				// 迴圈初始值不可為重複
+				for (int j = 0; j < dialogueData[i].dialogueContents.Length; j++)
 				{
-					// 等待玩家按下指定的按鍵 來繼續下段對話
-					while (!(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)))
+					// 第三個迴圈跑第i個對話資料的第j個對話內容中總共有幾個字
+					for (int k = 0; k < dialogueData[i].dialogueContents[j].Length; k++)
 					{
-						// null 為每一幀的時間
-						yield return null;
+						Debug.Log(dialogueData[i].dialogueContents[j][k]);
+						// 更新對話內容
+						textContent.text += dialogueData[i].dialogueContents[j][k];
+						// 打字間隔
+						yield return new WaitForSeconds(interval);
 					}
-				}
-				else if (isAutoplay)
-				{
-					continue;
-				}
-			}
-			else if (isAutoplay == true)
-			{
-				Debug.Log("自動播放中");
-				//yield return new WaitForSeconds(0.3f);
-			}
 
-			// 玩家按下繼續按鈕後 如果對話內容為空 則對話者名稱為空
-			if (textContent.text == "")
-			{
-				textTalker.text = "";
+					// 每段對話完成後顯示繼續圖示
+					continueIcon.SetActive(true);
+
+					// 如果 沒有隱藏對話框的話
+					if (isHideDialogue == false)
+					{
+						// 如果 沒有自動播放的話
+						if (isAutoplay == false)
+						{
+							// 等待玩家按下指定的按鍵 來繼續下段對話
+							while (!(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)))
+							{
+								// null 為每一幀的時間
+								yield return null;
+							}
+						}
+						else if (isAutoplay)
+						{
+							continue;
+						}
+					}
+					else if (isAutoplay == true)
+					{
+						Debug.Log("自動播放中");
+						//yield return new WaitForSeconds(0.3f);
+					}
+
+										// 玩家按下繼續按鈕後 清空對話內容
+					textContent.text = "";
+					// 隱藏繼續圖示
+					continueIcon.SetActive(false);
+					// 如果對話段落已結束 就關閉對話介面
+					//if (i == dialogueData.dialogueContents.Length - 1) dialogieUI.alpha = 0;
+				}
+				// 玩家按下繼續按鈕後 如果對話內容為空 則對話者名稱為空
+				if (textContent.text == "")
+				{
+					textTalker.text = "";
+				}
+
+				currentDialogueID = dialogueData[i].toDialogueID;
 			}
-			// 玩家按下繼續按鈕後 清空對話內容
-			textContent.text = "";
-			// 隱藏繼續圖示
-			continueIcon.SetActive(false);
-			// 如果對話段落已結束 就關閉對話介面
-			//if (i == dialogueData.dialogueContents.Length - 1) dialogieUI.alpha = 0;
+			else if (dialogueData[i].dialogueType == DialogueType.選項 && currentDialogueID == dialogueData[i].toDialogueID)
+			{
+				Debug.Log("<color=orange>這是「選項」</color>");
+				dialogueData[i].optionID = dialogueData[i].toDialogueID;
+				Debug.Log($"<color=orange>選項ID：{dialogueData[i].optionID}</color>");
+			}
 		}
 	}
 
