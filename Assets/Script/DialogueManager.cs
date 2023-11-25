f75b55fe04c3de4ed01d992b1f0154a4c32ec09c
Jack_Chiu <57485339+ploki0258@users.noreply.github.com>
2023-11-25 20:40:55 +0800

    Update 20231125修改對話紀錄系統

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -26,6 +26,7 @@ public class DialogueManager : MonoBehaviour
 	private GameObject autoplayButton = null;
 	[SerializeField, Header("自動播放按鈕顏色")]
 	private Color changeColor;
+
 	[SerializeField, Header("之前對話人名底圖")]
 	Image basemapTextTalkerBefore = null;
 	[SerializeField, Header("之前對話內容底圖")]
@@ -35,7 +36,7 @@ public class DialogueManager : MonoBehaviour
 	[SerializeField, Header("之前對話內容")]
 	TextMeshProUGUI textContentBefore = null;
 	[SerializeField, Header("之前對話紀錄")]
-	GameObject dialogueBeforePrefab = null;
+	GameObject dialogueLogPrefab = null;
 	[Header("底圖顏色列表")]
 	public Color[] basemapImages = new Color[0];
 
@@ -95,14 +96,17 @@ public class DialogueManager : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// 顯示之前對話記錄介面
+	/// </summary>
 	void ShowDialogueLogUI()
 	{
 		if (isHideDialogueLogUI == true)
 		{
+			DialogueSystem.instance.enabled = false;
 			isHideDialogueLogUI = false;
 			dialogieLogUI_CG.gameObject.SetActive(true);
 			dialogieLogUI_CG.alpha = 1f;
-			DialogueSystem.instance.enabled = false;
 		}/*
 		else if (Input.GetKeyDown(KeyCode.Escape) && isHideDialogueLogUI == false)
 		{
@@ -179,10 +183,25 @@ public class DialogueManager : MonoBehaviour
 	public void dialogueLogContent()
 	{
 		ShowDialogueLogUI();
-		textTalkerBefore.text = DialogueSystem.instance.dialogueData[0].dialogueTotalList[0].dialogueTalkerName;
-		//textContentBefore.text = DialogueSystem.instance.dialogueData[0].dialogueTotalList[0].dialogueContents[0].ToString();
-		basemapTextTalkerBefore.color = characterBasemapDic[DialogueSystem.instance.dialogueData[0].dialogueTotalList[0].dialogueTalkerName];
-		basemapTextContentBefore.color = characterBasemapDic[DialogueSystem.instance.dialogueData[0].dialogueTotalList[0].dialogueTalkerName];
+		for (int i = 0; i < DialogueSystem.instance.dialogueData.Length; i++)
+		{
+			for (int j = 0; j < DialogueSystem.instance.dialogueData[i].dialogueTotalList.Count; j++)
+			{
+				for (int k = 0; k < DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueContents.Length; k++)
+				{
+					if (DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueTalkerName == "")
+						return;
+
+					Instantiate(dialogueLogPrefab, dialogieLogUI.transform);
+
+					textTalkerBefore.text = DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueTalkerName;
+					textContentBefore.text = DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueContents[k].ToString();
+					basemapTextTalkerBefore.color = characterBasemapDic[DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueTalkerName];
+					//Debug.Log("回想人名：" + DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueTalkerName);
+					basemapTextContentBefore.color = characterBasemapDic[DialogueSystem.instance.dialogueData[i].dialogueTotalList[j].dialogueTalkerName];
+				}
+			}
+		}
 	}
 
 	public void CloseButton()
