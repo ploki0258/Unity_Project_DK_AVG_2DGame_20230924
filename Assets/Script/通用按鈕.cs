43dad4f8c9886dda8c75e8b8e2b8bced7abf2004
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2024-08-26 20:45:54 +0800

    Test 20240826

--- Assets/Script/通用按鈕.cs
+++ Assets/Script/通用按鈕.cs
@@ -1,7 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
 
 public class 通用按鈕 : MonoBehaviour
 {
+    [SerializeField] Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
